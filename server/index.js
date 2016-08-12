a9fff009514b356c6b8b84978ffb17481c77db82
Leonid Lygin <ionagamed@gmail.com>
2016-08-12 13:08:11 +0500

    remove unused junk

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ var app = express();
 app.use(express.static('client'));
 app.use('/lib', express.static('bower_components'));
 
-app.get('/', function (req, res) {
-    res.send('hello, world');
-});
-
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
     console.log('Application started on port ' + port);
