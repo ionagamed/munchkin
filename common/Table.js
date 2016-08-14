ade9086a461ff780fbce4e2af4808adc4708aaa3
Leonid Lygin <ionagamed@gmail.com>
2016-08-14 14:10:50 +0500

    fix fight tabs

--- common/Table.js
+++ common/Table.js
@@ -1,67 +1,67 @@
 /**
  * Created by tsmish on 13/08/16.
  */
-import { Card } from './Card'
-import { Player } from './Player'
+import {Card} from './Card'
+import {Player} from './Player'
 export class Table {
-	constructor() {
-		/** 
-		 * An array of players sitting on the table
-		 * 
-		 * @type [player] 
-		 */
-		this.players = [];
+    constructor() {
+        /**
+         * An array of players sitting on the table
+         *
+         * @type [player]
+         */
+        this.players = [];
 
-		/**
-		 * An array of currently active cards (in the middle of table)
-		 *
-		 * @type [string]
-		 */
-		this.activeCards = [];
+        /**
+         * An array of currently active cards (in the middle of table)
+         *
+         * @type [string]
+         */
+        this.activeCards = [];
 
-		/**
-		 * An array of discarded doors
-		 *
-		 * @type [string]
-		 */
-		this.discardedDoors = [];
+        /**
+         * An array of discarded doors
+         *
+         * @type [string]
+         */
+        this.discardedDoors = [];
 
         /**
          * An array of discarded treasure cards
-         * 
+         *
          * @type [string]
          */
         this.discardedTreasure = [];
 
         /**
-		 * Determines if there is an active game playing
-		 *
-		 * @type {bool}
-		 */
-
-		this.playing = false;
-
-		/**
-		 * Determines whose is the current turn
-		 * idx in {players}
-		 *
-		 * @type {string}
-		 */
-		this.turn = 0;
-
-		/**
-         * Currently going fight
-		 *
-		 * @type {Fight|null}
+         * Determines if there is an active game playing
+         *
+         * @type {bool}
          */
-		this.fight = null;
-	}
+
+        this.playing = false;
+
+        /**
+         * Determines whose is the current turn
+         * idx in {players}
+         *
+         * @type {string}
+         */
+        this.turn = 0;
+
+        /**
+         * Currently going fight
+         *
+         * @type {Fight|null}
+         */
+        this.fight = null;
+    }
 
     /**
      * Modifies the turn member so it points to the next player
      */
-	nextTurn() {
-		this.turn = (this.turn + 1) % this.players.length;
-	}
+    nextTurn() {
+        this.turn = (this.turn + 1) % this.players.length;
+    }
 }
 
