182f46bc9aa83ae6807edd883cd087dba8c191cb
Кирилл Климов < >
2016-08-24 11:40:31 +0500

    logic

---
+++ common/packs/pack1/doors/mate.js
@@ -0,0 +1,18 @@
+import { Card } from '../../../Card';
+import { Figth } from '../../../Figth';
+
+const id = 'out_to_lunch';
+
+class out_to_lunch extends Card {
+    constructor() {
+        super();
+        this.id = id;
+        this.pack = 'pack1';
+        this.kind = 'door';
+        this.type = 'out_to_lunch';
+    }
+    
+    Fight.monsters.push()
+    
+}
+Card.cards[id] = new out_to_lunch();
