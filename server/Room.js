0235dd51bf880cc1f1390f6fc6c34b0d5c46e125
Mickael Tsykalov <tsmish@tsmish.tk>
2016-08-24 14:10:06 +0500

    fix giveCards

--- server/Room.js
+++ server/Room.js
@@ -330,7 +330,7 @@ Room.giveCards = function(source, to, cardIds, room, method) {
                 return cardId;
         }
     });
-    to.hand.concat(cards);
+    to.hand = to.hand.concat(cards);
     switch (method) {
         case 'open':
             room.dispatch('gotCards', {
