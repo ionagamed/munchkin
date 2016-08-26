609781ac68768f6dd8d4ed3666abf99e6b4fc113
Кирилл Климов < >
2016-08-26 12:46:04 +0500

    logic

--- logic/packs/pack1/helpers/Race.js
+++ logic/packs/pack1/helpers/Race.js
@@ -7,17 +7,17 @@ import { Card } from '../../../Card';
 export class Race extends Card {
     canBeWielded(player, table) {
         if (player.hasCardWielded('half-breed')) {
-            return player.cardsOfTypeWielded('class') < 2;
+            return player.cardsOfTypeWielded('race') < 2;
         } else {
-            return player.cardsOfTypeWielded('class') < 1;
+            return player.cardsOfTypeWielded('race') < 1;
         }
     }
 
     canBeHeld(player, table) {
         if (player.hasCardWielded('half-breed')) {
-            return player.cardsOfTypeWielded('class') <= 2;
+            return player.cardsOfTypeWielded('race') <= 2;
         } else {
-            return player.cardsOfTypeWielded('class') <= 1;
+            return player.cardsOfTypeWielded('race') <= 1;
         }
     }
 }
