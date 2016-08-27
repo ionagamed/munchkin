3249fd2455e67f5dcaed595f6d2c815d1c236c6a
Mickael Tsykalov <tsmish@tsmish.tk>
2016-08-27 12:23:35 +0500

    fix Room

--- server/Room.js
+++ server/Room.js
@@ -519,8 +519,8 @@ Room.playerCommands['resurrect'] = (data, env) => {
     if(!env.player.dead) return;
     if(!env.table.playing) return;
     // env.player.hand = []
-    //     .concat(env.room.getCards('door', DOOR_BEGIN_COUNT)[0])
-    //     .concat(env.room.getCards('treasure', TREASURE_BEGIN_COUNT)[0]);
+    //     .concat(env.room.getCards('door', DOOR_BEGIN_COUNT))
+    //     .concat(env.room.getCards('treasure', TREASURE_BEGIN_COUNT));
     env.player.hand = ['3872_orcs', '1000_gold', 'acid_potion'];
     env.player.hand.map(cardId => {
         const card = Card.byId(cardId);
